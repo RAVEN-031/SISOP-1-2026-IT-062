43cb80a9671b56022448a7f742ce7b044d8378b9
RAVEN-031 <izzatwahyudi07@gmail.com>
2026-03-20 15:28:22 +0700

    Finished add tenant function for soal_3

--- soal_3/kost_slebew.sh
+++ soal_3/kost_slebew.sh
@@ -65,19 +65,44 @@ add_tenant() {
 	read -p "Nama Penghuni                : " name
 	read -p "Nomor Kamar                  : " num
 	read -p "Harga Sewa                   : " cost
-	read -p "Tanggal Masuk (YYYY-MM-DD)   : " date
+	read -p "Tanggal Masuk (YYYY-MM-DD)   : " inp_date
 	read -p "Status Awal (Aktif/Menunggak): " status
+
+	# Room number sanitation
 	flag=$(awk -v room=$num 'BEGIN {FS = ","} $2 == room {flag = 1} END {if (flag == 1) {print "1"} else {print "0"}}' ./data/penghuni.csv)
-	echo $flag
+	
 	if [[ $flag == "1" ]]
 	then
 		echo "Nomor Kamar Terpenuhi"
 		return 0 
-	elif [[ $cost < 0 ]]
+	
+	# Cost Sanitation
+	elif [[ $cost -lt 0 ]] # Note that other than equals/not equal to have different operator
 	then
 		echo "Harga sewa tidak boleh negatif"
 		return 0
+	
+	# Date Sanitation
+	elif [[ ! $inp_date =~ ^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$ ]]
+	then
+		echo "Format tanggal salah"
+		return 0
+	# Date Validation (too lazy, will just let the date be normalized)
+	elif [[ $(date -d "$inp_date" +%s) > $(date +%s) ]]
+	then
+		echo "Tanggal Invalid"
+		return 0
+	# Status Sanitation
+	elif [[ $status != "Aktif" && $status != "Menunggak" ]]
+	then
+		echo "Status Invalid"
+		return 0
+	else
+		awk -v a="$name" -v b="$num" -v c="$cost" -v d="$status" 'BEGIN {OFS=","} 1; END {print a,b,c,d}' data/penghuni.csv > data/penghuni.csv.temp
+		mv data/penghuni.csv.temp data/penghuni.csv
 	fi
+
+
 }
 
 
